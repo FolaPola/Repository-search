645297e194678695ba888620a38822b928f05c78
FolaPola <126721472+FolaPola@users.noreply.github.com>
2023-03-19 21:00:17 +0500

    Доработала поиск

--- js.js
+++ js.js
@@ -1,41 +1,34 @@
 'use strict';
 
-let commentName = document.getElementById('commentname');
+let inputElement = document.getElementById('inputElement');
 
-async function getRepos(repoName){
+document.getElementById('comment-add').onclick = function click(){
+    if (inputElement.value.length < 2) return alert('Слишком короткое значение');
+}
 
-    let response = await fetch(`https://api.github.com/search/repositories?q=${repoName}`);
+let formReprository = document.querySelector('.formReprository');
+formReprository.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    let valueInput = inputElement.value;
+    let response = await fetch(`https://api.github.com/search/repositories?q=${valueInput}`);
     
-    if (!response.ok) {
-        return null;
-    } else {
+    if (response.ok) {
         let result =  await response.json();
+        console.log(result);
         result = result.items.splice(0, 10);
 
-        let list = document.getElementById('comment');
+        let list = document.getElementById('responseGit');
         let key; 
 
         for (key  in result) {
-        let a = 'ссылка';
-
         list.innerHTML += `
         <ol class ='result'>
-        <h4 class="name">${result[key].name}</h4>
-        <a href="${result[key].url}">${a}</a>
+        <a href="${result[key].html_url}">${result[key].name}</a>
+        <h4 class="name"><span> Язык программирования:</span>${result[key].language}</h4>
         </ol>`
-        }
-        return list;
-    } 
-}
-
-
-document.getElementById('comment-add').onclick = function click(){
-    if (commentName.value.length < 2) {
-        return alert('Слишком короткое значение');
-    } else {  
-        return getRepos(commentName.value);
-    }
-}
-
-
-
+      
+    }} else {
+        alert("Ничего не найдено!")
+    }}
+)
